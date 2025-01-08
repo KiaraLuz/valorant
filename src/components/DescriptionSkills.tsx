e8a79c744262ba26b3acbc268248ce5326f0ea61
Huan <juan.murato.cruz@gmail.com>
2025-01-08 17:34:43 -0500

    feat: :sparkles: Add more data
    add api, interfaces, typos and more

--- src/components/DescriptionSkills.tsx
+++ src/components/DescriptionSkills.tsx
@@ -1,12 +1,13 @@
-export function DescriptionSkills() {
+interface Props {
+  title: string;
+  description: string;
+}
+
+export function DescriptionSkills({ title, description }: Props) {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="font-bold">Q-ALARMBOT</div>
-      <div>
-        EQUIP a covert Alarmbot. FIRE to deploy a bot that hunts down enemies
-        that get in range. After reaching its target, the bot explodes, applying
-        Vulernable. HOLD EQUIP to recall a deployed bot.
-      </div>
+    <div className='flex flex-col gap-4'>
+      <div className='font-bold'>{title}</div>
+      <div>{description}</div>
     </div>
   );
 }
